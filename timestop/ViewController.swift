ce95fe53a0a10f767cb5f5f11bb2b00d16136c84
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-04-16 15:17:39 +0900

    timestop

--- timestop/ViewController.swift
+++ timestop/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    var myTimer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timeLabel.text = formatter.string(from:myDatePicker.date)
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updatTime()
+        })
     }
-
+    func updatTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        currentTimeLabel.text = formatter.string(from: date)
+        
+        if currentTimeLabel.text == timeLabel.text {
+            view.backgroundColor = UIColor.red
+        }
+    }
+    
+    @IBAction func changeDatePicker(_ sender: Any) {
+        print("change Date Pucker")
+        print(myDatePicker.date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM-dd HH:mm:ss EE"
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timeLabel.text = formatter.string(from:myDatePicker.date)
+    }
+    
+    @IBAction func stopAlert(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
